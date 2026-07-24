0dcaf792211cbdd987e9b87bf9e7cc0f3c17bada
Muthulakshmi Balasubramanian <muthulakshmi020504@gmail.com>
2026-07-24 11:56:36 +0530

    Add VariablesAndDataTypes program

---
+++ Basics/VariablesAndDataTypes.java
@@ -0,0 +1,16 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        // Different data types in Java
+        int age = 21;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Muthulakshmi";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
