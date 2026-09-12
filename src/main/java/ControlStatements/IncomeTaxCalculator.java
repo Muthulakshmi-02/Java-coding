46bb6d5fcd13cd2e506c17dca77fa6b5822a988e
Muthulakshmi B <balamuthu795@gmail.com>
2026-09-13 04:27:02 +0530

    Add Under ControlStatements.

---
+++ src/main/java/ControlStatements/IncomeTaxCalculator.java
@@ -0,0 +1,28 @@
+package ControlStatements;
+
+import java.util.Scanner;
+
+public class IncomeTaxCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter annual income: ");
+        double income = sc.nextDouble();
+
+        double tax;
+
+        if (income <= 250000) {
+            tax = 0;
+        } else if (income <= 500000) {
+            tax = income * 0.05;
+        } else if (income <= 1000000) {
+            tax = income * 0.20;
+        } else {
+            tax = income * 0.30;
+        }
+
+        System.out.println("Income Tax = ₹" + tax);
+
+        sc.close();
+    }
+}
