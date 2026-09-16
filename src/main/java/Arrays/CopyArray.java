f66a660442c14b3d3afd2768f965ce2a7945c07f
Muthulakshmi Balasubramanian <muthulakshmi020504@gmail.com>
2026-09-16 20:49:01 +0530

    Add CopyArray program

---
+++ src/main/java/Arrays/CopyArray.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int size = sc.nextInt();
+
+        int[] arr1 = new int[size];
+        int[] arr2 = new int[size];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < size; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < size; i++) {
+            arr2[i] = arr1[i];
+        }
+
+        System.out.println("Copied Array Elements:");
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+
+        sc.close();
+    }
+}
