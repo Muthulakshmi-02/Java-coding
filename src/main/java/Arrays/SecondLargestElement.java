cc736fcefaf46db2b460b552b91b745fc29b6a67
Muthulakshmi Balasubramanian <muthulakshmi020504@gmail.com>
2026-09-16 20:46:03 +0530

    Add SecondLargestElement program

---
+++ src/main/java/Arrays/SecondLargestElement.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Largest Element = " + largest);
+        System.out.println("Second Largest Element = " + secondLargest);
+
+        sc.close();
+    }
+}
