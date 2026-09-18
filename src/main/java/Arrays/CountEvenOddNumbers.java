4aa004e3ddfc55e2eadac283d25fef993b6f4d67
Muthulakshmi Balasubramanian <muthulakshmi020504@gmail.com>
2026-09-18 14:42:16 +0530

    Add CountEvenOddNumbers  program

---
+++ src/main/java/Arrays/CountEvenOddNumbers.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class CountEvenOddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Number of Even Elements = " + evenCount);
+        System.out.println("Number of Odd Elements = " + oddCount);
+
+        sc.close();
+    }
+}
